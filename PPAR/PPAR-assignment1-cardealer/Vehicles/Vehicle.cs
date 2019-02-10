508c7f61da291983829210469b202485e60923f4
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-02-10 22:49:39 +0100

    added some property attributes

--- PPAR/PPAR-assignment1-cardealer/Vehicles/Vehicle.cs
+++ PPAR/PPAR-assignment1-cardealer/Vehicles/Vehicle.cs
@@ -1,12 +1,14 @@
+using Newtonsoft.Json;
+
 namespace PPAR_assignment1_cardealer.Vehicles
 {
     public abstract class Vehicle
     {
         private static int nextVehicleId = 0;
-        public int Id { get; }
-        public double Price { get; set; }
-        public string LicensePlate { get; set; }
-        public string Vendor { get; set; }
+        [JsonProperty] public int Id { get; }
+        [JsonProperty] [VehicleProperty] public double Price { get; set; } = 0;
+        [JsonProperty] [VehicleProperty] public string LicensePlate { get; set; } = "";
+        [JsonProperty] [VehicleProperty] public string Vendor { get; set; } = "";
 
         public Vehicle()
         {
