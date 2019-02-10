c192145d92e2983c5c25a8f8beaed042723f3c0d
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-02-10 22:50:16 +0100

    made dynamic property setting possible

--- PPAR/PPAR-assignment1-cardealer/Commands/CreateVehicleCommand.cs
+++ PPAR/PPAR-assignment1-cardealer/Commands/CreateVehicleCommand.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using PPAR_assignment1_cardealer.Vehicles;
@@ -7,12 +8,51 @@ namespace PPAR_assignment1_cardealer.Commands
 {
     public class CreateVehicleCommand : IMenuCommand
     {
+        private static object parseToPrimitiveType(string value, Type t)
+        {
+            if (!t.IsPrimitive)
+            {
+                throw new InvalidCastException("Cannot parse a type which is not a primitive type!");
+            }
+
+            if (t.Equals(typeof(int)))
+            {
+                return int.Parse(value);
+            }
+            else if (t.Equals(typeof(long)))
+            {
+                return long.Parse(value);
+            }
+            else if (t.Equals(typeof(short)))
+            {
+                return short.Parse(value);
+            }
+            else if (t.Equals(typeof(char)))
+            {
+                return char.Parse(value);
+            }
+            else if (t.Equals(typeof(float)))
+            {
+                return float.Parse(value);
+            }
+            else if (t.Equals(typeof(double)))
+            {
+                return double.Parse(value);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
         public void Execute(CarDealer dealer)
         {
-            var vehicleTypes = Assembly.GetExecutingAssembly().GetTypes().Where(t => t.BaseType == typeof(Vehicle)).ToArray();
+            var vehicleTypes = Assembly.GetExecutingAssembly().GetTypes().Where(t => t.BaseType == typeof(Vehicle))
+                .ToArray();
 
             Vehicle v;
-            
+            IEnumerable<PropertyInfo> vehicleProperties;
+
             while (true)
             {
                 Console.Write("Select vehicle kind(");
@@ -24,7 +64,7 @@ namespace PPAR_assignment1_cardealer.Commands
                         Console.Write(", ");
                     }
                 }
-            
+
                 Console.Write("): ");
                 string input = Console.ReadLine();
                 int index;
@@ -32,12 +72,39 @@ namespace PPAR_assignment1_cardealer.Commands
                 if (!int.TryParse(input, out index)) continue;
                 if (index < 0 || index >= vehicleTypes.Length) continue;
 
-                v = (Vehicle)Activator.CreateInstance(vehicleTypes[index]);
+                v = (Vehicle) Activator.CreateInstance(vehicleTypes[index]);
+                vehicleProperties = vehicleTypes[index].GetProperties()
+                    .Where(x => Attribute.IsDefined(x, typeof(VehicleProperty)));
 
                 break;
             }
 
-            while (true)
+            foreach (var vehicleProperty in vehicleProperties)
+            {
+                while (true)
+                {
+                    Console.Write($"Please enter {vehicleProperty.Name}: ");
+                    string input = Console.ReadLine();
+                    if (vehicleProperty.PropertyType.Equals(typeof(string)))
+                    {
+                        vehicleProperty.SetValue(v, input);
+                        break;
+                    }
+                    else
+                    {
+                        try
+                        {
+                            vehicleProperty.SetValue(v, parseToPrimitiveType(input, vehicleProperty.PropertyType));
+                            break;
+                        }
+                        catch (FormatException e)
+                        {
+                        }
+                    }
+                }
+            }
+
+            /*while (true)
             {
                 Console.Write("Enter price: ");
                 string input = Console.ReadLine();
@@ -51,8 +118,8 @@ namespace PPAR_assignment1_cardealer.Commands
             Console.Write("Enter license plate: ");
             v.LicensePlate = Console.ReadLine();
             Console.Write("Enter vendor: ");
-            v.Vendor = Console.ReadLine();
-            
+            v.Vendor = Console.ReadLine();*/
+
             dealer.AddVehicle(v);
         }
     }
