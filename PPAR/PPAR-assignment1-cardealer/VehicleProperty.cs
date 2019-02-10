a31eab22ad8bf0c7c557728c2db533f209c4d87e
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-02-10 22:51:21 +0100

    added vehicle property class

---
+++ PPAR/PPAR-assignment1-cardealer/VehicleProperty.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace PPAR_assignment1_cardealer
+{
+    [AttributeUsage(AttributeTargets.Property, Inherited = true)]
+    public class VehicleProperty : Attribute
+    {
+        
+    }
+}
