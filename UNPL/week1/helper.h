88924fa454cf2a8ca2633ab0ce29711aa5a79b0c
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-02-12 20:49:02 +0100

    added line break trimming

--- UNPL/week1/helper.h
+++ UNPL/week1/helper.h
@@ -6,13 +6,22 @@ typedef unsigned char bool;
 #define true 1
 
 char* read_from_console(int number_of_charcters) {
-  char* buffer = (char*)malloc(sizeof(char) * number_of_charcters);
+  char* buffer = (char*)malloc(sizeof(char) * (number_of_charcters + 1));
   memset(buffer, 0, sizeof(buffer));
   int number = 0;
 
   if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
     fprintf(stderr, "Error in fgets()\n");
     exit(EXIT_FAILURE);
+  }
+
+  for (int i = number_of_charcters; i != 0; --i) {
+    if (buffer[i] == '\n') {
+      buffer[i] = '\0';
+      if (buffer[i - 1] == '\r') {
+        buffer[i - 1] = '\0';
+      }
+    }
   }
 
   return buffer;
