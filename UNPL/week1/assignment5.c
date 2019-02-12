90177ab3eb025e09f4cb736c7e33d2dfa3cd97da
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-02-12 19:10:39 +0100

    replaced bitshifts with bitmasks

--- UNPL/week1/assignment5.c
+++ UNPL/week1/assignment5.c
@@ -4,12 +4,12 @@
 unsigned long int datumcode(int jaar, int maand, int dag);
 
 int main() {
-  printf("%lu\r\n", datumcode(1970, 1, 100));
+  printf("%lu\r\n", datumcode(4030, 11, 30));
 }
 
 unsigned long int datumcode(int jaar, int maand, int dag) {
-  unsigned long int result = (dag << 27) >> 27;
-  result += ((maand << 28) >> 28) << 5;
-  result += ((jaar << 20) >> 20) << 9;
+  unsigned long int result = dag & 0b11111;
+  result += maand & 0b1111 << 5;
+  result += jaar << 9;
   return result;
 }
