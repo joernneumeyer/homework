5f9c1919d370261d7223d7d3162df60dde126ec3
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-02-24 22:02:40 +0100

    added concrete lecturer class

---
+++ PPAR/user-administration/Users/Lecturer.cs
@@ -0,0 +1,9 @@
+namespace user_administration.Users
+{
+    public class Lecturer : User
+    {
+        public string PhoneNumber { get; set; }
+        public string Abbreviation { get; set; }
+        public string DateStarted { get; set; }
+    }
+}
