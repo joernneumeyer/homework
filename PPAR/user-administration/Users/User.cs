7147b512dfa37fbdd92823020100889f2f54f112
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-02-26 22:56:01 +0100

    added attributes and email validation (if not empty)

--- PPAR/user-administration/Users/User.cs
+++ PPAR/user-administration/Users/User.cs
@@ -1,13 +1,47 @@
+using System;
+using System.Text.RegularExpressions;
 using user_administration.Persistence;
 
 namespace user_administration.Users
 {
     public abstract class User : IPersistableEntity
     {
+        private static int idCounter = 0;
+        
+        private static Regex emailRegex = new Regex(@"^[a-zA-Z-_0-9]+@[a-zA-Z-_0-9]+\.[a-zA-Z]+$");
+
+        public User()
+        {
+            this.Id = ++idCounter;
+        }
+        
+        [PersistedAttribute("last_name")]
         public string LastName { get; set; }
+        [PersistedAttribute("first_name")]
         public string FirstName { get; set; }
-        public string Email { get; set; }
+
+        private string email;
+        [PersistedAttribute("email")] 
+        public string Email
+        {
+            get { return this.email; }
+            set
+            {
+                if (value == "")
+                {
+                    this.email = "";
+                    return;
+                }
+                if (!emailRegex.IsMatch(value))
+                {
+                    throw new ArgumentException($"E-Mail has to match pattern {emailRegex.ToString()}! Given E-Mail was {value}");
+                }
+                this.email = value;
+            }
+        }
+        [PersistedAttribute("nationality")]
         public string Nationality { get; set; }
-        public int Id { get; set; } = 0;
+        [PersistedAttribute("id")]
+        public int Id { get; set; }
     }
 }
