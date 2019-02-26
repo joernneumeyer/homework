2847b3477732031abeb1a6071a4e0cc0a84e1cd4
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-02-26 22:54:03 +0100

    removed obsolete interface

--- PPAR/user-administration/IAdministrationCommand.cs
+++
@@ -1,9 +0,0 @@
-using user_administration.Persistence;
-
-namespace user_administration
-{
-    public interface IAdministrationCommand
-    {
-        void Execute(IPersistenceService connection);
-    }
-}
