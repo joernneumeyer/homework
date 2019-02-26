70df3227f3fa5ca1ac18617a0f9d672b42309300
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-02-26 22:56:18 +0100

    added annotations

--- PPAR/user-administration/Users/Student.cs
+++ PPAR/user-administration/Users/Student.cs
@@ -2,8 +2,11 @@ namespace user_administration.Users
 {
     public class Student : User
     {
+        [PersistedAttribute("study_program")]
         public string StudyProgram { get; set; }
+        [PersistedAttribute("cohort")]
         public string Cohort { get; set; }
+        [PersistedAttribute("class")]
         public string Class { get; set; }
     }
 }
