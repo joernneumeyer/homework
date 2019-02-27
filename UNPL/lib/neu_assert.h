b2e937117050746198ab773f20d03bd58f346046
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-02-27 11:38:41 +0100

    added comments and macros for equal assertion

--- UNPL/lib/neu_assert.h
+++ UNPL/lib/neu_assert.h
@@ -1,7 +1,18 @@
 #ifndef NEU_ASSERT_H
 #define NEU_ASSERT_H 1
-#include "types.h"
+#include "neu_types.h"
 
+/**
+ * Makes an assertion and specifies an error message.
+ */
 void assert(boolean condition, cstring message);
+/**
+ * Makes an assertion, that the two passed values are equal.
+ */
+#define assert_equals(val1, val2) assert(val1 == val2, sprintf("Equal assertion failed in line %d!", __LINE__))
+/**
+ * Makes an assertion, that the two passed values are equal and specifies an error message.
+ */
+#define assert_equals_m(val1, val2, message) assert(val1 == val2, message)
 
 #endif
