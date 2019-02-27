a88d30c7552e452a5ccc8b2fea33a6c7ffa3b88f
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-02-27 11:39:19 +0100

    added dummy implementation for assert; added linked list struct to store error messages

---
+++ UNPL/lib/neu_assert.c
@@ -0,0 +1,15 @@
+#include "neu_assert.h"
+#include "neu_memory.h"
+
+struct neu_assert_node {
+  cstring message;
+  struct neu_assert_node* next;
+};
+
+struct neu_assert_node* make_neu_assert_node() {
+  return neu(struct neu_assert_node, 1);
+}
+
+void assert(boolean condition, cstring message) {
+
+}
