8a33bb1678cfcae5a0a9a1f5d8bedba694b415f1
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-02-27 13:18:58 +0100

    adjusted naming and typedef; implemented assert and assert init function

--- UNPL/lib/neu_assert.c
+++ UNPL/lib/neu_assert.c
@@ -1,15 +1,25 @@
 #include <neu_assert.h>
 #include <neu_memory.h>
+#include <neu_types.h>
 
-struct neu_assert_node {
+typedef struct neu_assert_node {
   cstring message;
   struct neu_assert_node* next;
-};
+} neu_assert_node_t;
 
-struct neu_assert_node* make_neu_assert_node() {
-  return neu(struct neu_assert_node, 1);
+neu_assert_node_t* neu_assert_node_make() {
+  return neu(neu_assert_node_t, 1);
 }
 
-void assert(boolean condition, cstring message) {
+neu_assert_node_t* neu_assert_message_head = NULL;
 
+void neu_assert_init() {
+  neu_assert_message_head = neu_assert_node_make();
+}
+
+void neu_assert(boolean condition, cstring message) {
+  if (condition) return;
+  neu_assert_node_t* assert_message = neu_assert_node_make();
+  assert_message->message = message;
+  neu_assert_message_head->next = assert_message;
 }
