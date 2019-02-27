66934d19ac20f3bcecd9cc3386d7085a2a7e10cb
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-02-27 14:35:34 +0100

    added assertion for bigger than; moved default message into separat macro

--- UNPL/lib/neu_assert.h
+++ UNPL/lib/neu_assert.h
@@ -13,14 +13,23 @@ void neu_assert_init();
 void neu_assert(boolean condition, cstring message);
 
 void neu_assert_report();
-
-/**
+#define neu_assert_assertion_error_message_default char* neu_assert_message_buffer = neu_alloc(char, 150); { memset(neu_assert_str_buffer, 0, sizeof(char) * 150); sprintf(neu_assert_str_buffer, "Assertion in file '%s' on line %d failed!", __FILE__, __LINE__); }
+/*
  * Makes an assertion, that the two passed values are equal.
  */
-#define neu_assert_equals(val1, val2) { char* neu_assert_str_buffer = neu_alloc(char, 150); memset(neu_assert_str_buffer, 0, sizeof(char) * 150); sprintf(neu_assert_str_buffer, "Equal assertion failed in file '%s' in line %d!", __FILE__, __LINE__); neu_assert(val1 == val2, neu_assert_str_buffer);}
+#define neu_assert_equals(val1, val2) { neu_assert_assertion_error_message_default; neu_assert(val1 == val2, neu_assert_message_buffer);}
 /**
  * Makes an assertion, that the two passed values are equal and specifies an error message.
  */
 #define neu_assert_equals_m(val1, val2, message) neu_assert(val1 == val2, message)
+
+/*
+ * Makes an assertion, that the first value is larger than the second value.
+ */
+#define neu_assert_bigger_than(val1, val2) { neu_assert_assertion_error_message_default; neu_assert(val1 > val2, neu_assert_message_buffer);}
+/**
+ * Makes an assertion, that the first value is larger than the second value and specifies an error message.
+ */
+#define neu_assert_bigger_than_m(val1, val2, message) neu_assert(val1 > val2, message)
 
 #endif
