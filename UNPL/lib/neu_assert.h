9aeb61e38ec3409517f1b635c786c1e2f3cd52a9
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-02-27 13:18:11 +0100

    added initialization function definition and adjusted naming

--- UNPL/lib/neu_assert.h
+++ UNPL/lib/neu_assert.h
@@ -2,17 +2,19 @@
 #define NEU_ASSERT_H 1
 #include <neu_types.h>
 
+void neu_assert_init();
+
 /**
  * Makes an assertion and specifies an error message.
  */
-void assert(boolean condition, cstring message);
+void neu_assert(boolean condition, cstring message);
 /**
  * Makes an assertion, that the two passed values are equal.
  */
-#define assert_equals(val1, val2) assert(val1 == val2, sprintf("Equal assertion failed in line %d!", __LINE__))
+#define neu_assert_equals(val1, val2) neu_assert(val1 == val2, sprintf("Equal assertion failed in line %d!", __LINE__))
 /**
  * Makes an assertion, that the two passed values are equal and specifies an error message.
  */
-#define assert_equals_m(val1, val2, message) assert(val1 == val2, message)
+#define neu_assert_equals_m(val1, val2, message) neu_assert(val1 == val2, message)
 
 #endif
