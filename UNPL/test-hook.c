4a3de59ce19fdbd51a88141a832b6cd8b7cd211b
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-03-17 22:52:15 +0100

    added proper test invocation and setup

--- UNPL/test-hook.c
+++ UNPL/test-hook.c
@@ -1,7 +1,14 @@
 #include <neu_assert.h>
 
+/**
+ * Dummy definition for the function which is called
+ * by the library in order to register all tests.
+ */
+void neu_assert_register_tests();
+
 int main() {
   neu_assert_init();
-  neu_assert_equals(1, 2);
+  neu_assert_register_tests();
+  neu_assert_run();
   neu_assert_report();
 }
