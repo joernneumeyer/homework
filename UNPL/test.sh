45957239545739ced06a4ade187b60d9c2783c40
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-03-18 00:09:02 +0100

    finished implementation of automatic test script

--- UNPL/test.sh
+++ UNPL/test.sh
@@ -1,6 +1,45 @@
 #!/usr/bin/env bash
+
+set -e
+
+if [[ ! -d $1 ]]
+then
+  echo "Please provide a valid test folder!"
+  exit
+fi
+
 cd lib
 make > /dev/null
 cd ..
-gcc test-hook.c -o test.o -lneu -Llib -Ilib > /dev/null
+
+TEST_FILES=$(find $1 -name "*.test.h")
+
+TEST_SOURCE=$(printf "#include <neu_assert.h>\r\n")
+
+for test_file in $TEST_FILES
+do
+ TEST_SOURCE="${TEST_SOURCE}#include \"${test_file}\"$(printf '\r\n')"
+done
+
+TEST_SOURCE="${TEST_SOURCE}$(echo "int main() { neu_assert_init();")"
+
+for test_file in $TEST_FILES
+do
+  functions=$(cat $test_file | grep -Eo "void ([a-zA-Z_])+" | echo $(read arr; set $arr; echo $2))
+  for func in $functions
+  do
+    TEST_SOURCE="${TEST_SOURCE}neu_assert_add_test($func);"
+  done
+  #gcc $test_file -o testlib.o -lneu -Llib -Ilib -c
+  #gcc test-hook.c testlib.o -o test.o -lneu -Llib -Ilib > /dev/null
+  #./test.o
+done
+
+TEST_SOURCE="${TEST_SOURCE}neu_assert_run();
+  neu_assert_report();
+}"
+
+echo ${TEST_SOURCE} > test.c
+
+gcc -o test.o -lneu -Llib -Ilib -I. test.c
 ./test.o
