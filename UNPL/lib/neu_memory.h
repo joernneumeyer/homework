bbdec3828fe8d0a1a683e48d1977a86fb9783343
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-03-18 11:52:58 +0100

    moved library into submodule

--- UNPL/lib/neu_memory.h
+++
@@ -1,8 +0,0 @@
-#ifndef NEU_MEMORY_H
-#define NEU_MEMORY_H 1
-#include <stdlib.h>
-
-#define neu_malloc(type, factor) (type*)malloc(sizeof(type) * factor)
-#define neu_calloc(type, factor) (type*)calloc(factor, sizeof(type))
-
-#endif
